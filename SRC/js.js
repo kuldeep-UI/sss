b62da71558b48e55c553da2b220af093c496d348
kuldeep <skuldeep866@gmail.com>
2022-11-16 15:36:55 +0530

    ppp

--- SRC/js.js
+++ SRC/js.js
@@ -1,10 +1,13 @@
-<script>    
-$('#ChangeToggle').click(function () {
-    if($('#ChangeToggle span').hasClass('ToggleButton')) {
-        $('#ChangeToggle').html('<span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>'); 
-    }
-    else {      
-        $('#ChangeToggle').html('<span class="ToggleButton glyphicon glyphicon-remove"></span>'); 
-    }
-});
-</script>
+
+
+// <!-- preloder -->
+
+
+  $(window).on("load", function(){
+    $(".preloder").fadeOut(2000);
+  });
+
+
+// counter
+// <!-- counter -->
+
